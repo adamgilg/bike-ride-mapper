4d6fca9bcecd8530163e7804045d79c9d44201ad
Victor Oleinikov and Erik Trautman <erik+victor@appacademy.io>
2013-02-07 09:45:35 -0800

    rails project with testing dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeRides::Application.config.secret_token = 'c7b93da70b42338e45ecdaf265570661bfd9a9d043fa772a907df4d0d27d8cfac0f79de70e006f9d681f91f690e08905a75da888c97272ee2747124711270eba'
